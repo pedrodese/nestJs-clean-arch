876a85af9c81d8acaefa3905cc5ed7e1159659bc
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-05-19 00:26:32 -0300

    added a new endpoint for the status of application

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  getApplicationStatus(): string {
+    return 'Application Up!'
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
